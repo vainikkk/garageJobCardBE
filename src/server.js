dc3785fd5f5084cc617060f90dd4b52a81fdcf60
vainikkk <vainik.kakadia@gmail.com>
2025-05-16 16:58:15 -0400

    host public changes

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ await fastify.register(serviceRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-fastify.listen({ port: PORT }, (err, address) => {
+fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
